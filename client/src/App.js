beb225465b2faf6cc7af705c1f25b8ca4f1dcfe0
Alberto Marin <albertomr84@gmail.com>
2020-05-30 12:32:29 +0100

    finishing app

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Launches from './components/Launches';
+import Launch from './components/Launch';
 import logo from './spacex-vector-logo.png';
 import './App.css';
 
@@ -12,17 +14,20 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='App'>
-        <header className='container'>
-          <img
-            src={logo}
-            className='App-logo'
-            alt='logo SpaceX'
-            style={{ width: 300, display: 'block', margin: 'auto' }}
-          />
-          <Launches />
-        </header>
-      </div>
+      <Router>
+        <div className='App'>
+          <header className='container'>
+            <img
+              src={logo}
+              className='App-logo'
+              alt='logo SpaceX'
+              style={{ width: 300, display: 'block', margin: 'auto' }}
+            />
+            <Route exact path='/' component={Launches} />
+            <Route exact path='/launch/:flight_number' component={Launch} />
+          </header>
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
